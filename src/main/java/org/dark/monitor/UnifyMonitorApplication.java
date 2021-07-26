75447e4e4bd31a978c1d44992c052029001aab78
tinyWolf <189192663@qq.com>
2021-07-26 22:01:09 +0800

    Initial commit

---
+++ src/main/java/org/dark/monitor/UnifyMonitorApplication.java
@@ -0,0 +1,13 @@
+package org.dark.monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnifyMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnifyMonitorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/dark/monitor/UnifyMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dark.monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnifyMonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
